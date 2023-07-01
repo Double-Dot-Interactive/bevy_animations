ffc8acdc8e554033a8a2ecfd6a971eec66dd063b
Aaron <aaron@pop-os.localdomain>
2023-06-30 20:10:48 -0500

    feat: updated src files from rusty_farm

---
+++ plugins.rs
@@ -0,0 +1,226 @@
+use bevy::{prelude::*};
+
+use crate::bevy_animations::*;
+
+/// A plugin for Bevy that adds support for animations.
+/// You Can Add This Plugin To Your Bevy Game Like This
+/// ```
+/// use bevy_animations::AnimationsPlugin;
+/// use bevy::prelude::*;
+/// 
+/// fn main() {
+///     App::new()
+///         .add_plugins(DefaultPlugins)
+///         .add_plugins(AnimationsPlugin {
+///             pixels_per_meter: 20. // your desired pixels_per_meter
+///         })
+///         .run()
+/// }
+/// ```
+/// Note that the `pixels_per_meter` field will be used for your [`TransformAnimation`](crate::animations::TimedAnimation)
+#[derive(Debug, Default)]
+pub struct AnimationsPlugin {
+    /// The number of pixels per meter.
+    pub pixels_per_meter: f32,
+}
+
+impl Plugin for AnimationsPlugin {
+    /// Builds the plugin.
+    fn build(&self, app: &mut App) {
+        app
+            .insert_resource(AnimationsConfig {
+                pixels_per_meter: self.pixels_per_meter,
+            })
+            .add_event::<AnimationEvent>()
+            .add_event::<ResetAnimationEvent>()
+            .insert_resource(Animations::default())
+            .insert_resource(EntitesToRemove::default())
+            .add_system_set(SystemSet::new()
+                .with_system(catch_animation_event.label("events").before("remove"))
+                .with_system(catch_reset_events.label("reset").after("events"))
+                .with_system(remove_entites.label("remove").after("reset"))
+            );
+    }
+}
+
+/// Main System That Checks for Incoming events
+/// If any incoming events are found they are checked to make sure they are new and if they are the Handle<TextureAtlas> is changed for the entity
+/// After checking the events. All the [`AnimatingEntities`](crate::AnimatingEntities) have their current animations cycled
+fn catch_animation_event(
+    time: Res<Time>,
+    mut query: Query<(
+        &mut Handle<TextureAtlas>,
+        &mut TextureAtlasSprite,
+        &mut Transform,
+        &AnimationDirection
+    )>,
+    mut animations: ResMut<Animations>,
+    mut entities_to_remove: ResMut<EntitesToRemove>,
+    config: Res<AnimationsConfig>,
+    mut animation_events: EventReader<AnimationEvent>
+) {
+    // our main event loop
+    for event in animation_events.iter() {
+        // get the animating entity from the entity passed in from our event
+        let animating_entity = animations.entities.get_mut(&event.1).expect(format!("Entity Not Found in Map For {} animation make sure your adding every necessary component to the entity i.e `AnimationDirection`", event.0).as_str());
+        // query the texture the sprite and the current direction of the entity
+        let (mut texture_atlas, mut sprite, _, direction) = match query.get_mut(animating_entity.entity) {
+            Ok(handle) => handle,
+            Err(_) => {
+                // if we didn't find the entity from the query it doesn't exist anymore and should be removed via the remove_entites system
+                entities_to_remove.0.push(event.1);
+                continue;
+            }
+        };
+        // if incoming event is new 
+        if animating_entity.curr_animation.lock().unwrap().get_name() != event.0 {
+            // get the Arc pointer for the animation
+            let new_animation_arc = animating_entity.animations.get(event.0).expect(format!("No Animation Found For `{}` make sure the name matches your configuration", event.0).as_str());
+            // unlock the animation if we don't do this we will hit a deadlock whenever we try to unlock the Arc<Mutex<>>
+            let mut new_animation = new_animation_arc.lock().unwrap();
+            let mut blocking = false;
+            let mut new_priority = 0;
+            let mut sprite_index = 0;
+            // get the temp variables above so we don't need to twice
+            if let Some(new_timed_animation) = new_animation.timed_animation() {
+                blocking = new_timed_animation.blocking;
+                new_priority = new_timed_animation.blocking_priority;
+                sprite_index = new_timed_animation.sprite_index(&animating_entity.last_valid_direction);
+            }
+            // if the new animation isn't a timed one we don't care about blocking or priority
+            else if let Some(new_transform_animation) = new_animation.transform_animation() {
+                sprite_index = new_transform_animation.sprite_index(&animating_entity.last_valid_direction);
+            }
+            // if we are in a blocking animation we don't want to changed our animation state
+            if animating_entity.in_blocking_animation {
+                // check the new animations priority from the current one
+                if let Some(curr_timed_animation) = animating_entity.curr_animation.lock().unwrap().timed_animation() {
+                    if curr_timed_animation.blocking_priority > new_priority {
+                        continue;
+                    }
+                }
+                else {
+                    continue;
+                }
+            }
+            animating_entity.curr_animation.lock().unwrap().reset_animation();
+            animating_entity.curr_animation = new_animation_arc.clone();
+            animating_entity.in_blocking_animation = blocking;
+            
+            sprite.index = sprite_index;
+            *texture_atlas = new_animation.get_atlas();
+        }
+        animating_entity.curr_animation_called = true;
+        // if our direction is changed we can set the current direction
+        if animating_entity.curr_direction != *direction {
+            animating_entity.curr_direction = direction.clone();
+            // we don't want to set a Still direction to our last valid direction field because our animations won't be right
+            if *direction != AnimationDirection::Still {
+                animating_entity.last_valid_direction = direction.clone();
+            }
+        }
+    } 
+    // our main animating loop
+    for (entity, animation_entity) in animations.entities.iter_mut() {
+        // query the sprite and transform from the entity
+        let (_, sprite, transform, _) = match query.get_mut(*entity) {
+            Ok(query) => query,
+            Err(_) => {
+                // if we didn't find the entity in the query we should remove it as it doesn't exist anymore
+                entities_to_remove.0.push(*entity);
+                continue;
+            }
+        };
+        // unlock the current animation once so we don't hit a deadlock
+        let mut curr_animation = animation_entity.curr_animation.lock().unwrap();
+        let name = curr_animation.get_name();
+
+        // if the current animation wasn't started via an `AnimationEvent`
+        if !animation_entity.curr_animation_called {
+            continue;
+        }
+
+        // if the current animation is transform based we should cycle it
+        if let Some(transform_animation) = curr_animation.transform_animation() {
+            if animation_entity.in_blocking_animation {
+                continue;
+            }
+            if let None = transform_animation.cycle_animation(sprite, &animation_entity.last_valid_direction, transform, config.pixels_per_meter, name) {
+                animation_entity.curr_animation_called = false;
+            }
+        } 
+        // if our current animation is timed based we should cycle it
+        else if let Some(timed_animation) = curr_animation.timed_animation() {
+            if let None = timed_animation.cycle_animation(sprite, &animation_entity.last_valid_direction, time.delta(), name) {
+                animation_entity.in_blocking_animation = false;
+                animation_entity.curr_animation_called = false;
+            }
+        }
+        // if the current animation is linear time based we should cycle it
+        else if let Some(linear_timed_animation) = curr_animation.linear_timed_animation() {
+            if let None = linear_timed_animation.cycle_animation(sprite, time.delta(), name) {
+                animation_entity.in_blocking_animation = false;
+                animation_entity.curr_animation_called = false;
+            }
+        }
+        // if we get here something bad happened it will most likely never hit as the typing is pretty strong
+        else {
+            panic!("Something Went Terribly Wrong Animating {} Check Your Configurations", curr_animation.get_name());
+        } 
+    }
+}
+
+fn catch_reset_events(
+    mut query: Query<(
+        &mut TextureAtlasSprite,
+        &AnimationDirection
+    )>,
+    mut animations: ResMut<Animations>,
+    mut entities_to_remove: ResMut<EntitesToRemove>,
+    mut animation_events: EventReader<ResetAnimationEvent>
+) {
+    for event in animation_events.iter() {
+        // if the entity wasn't found in the query we want to remove it from our data structure
+        let (mut sprite, direction) = match query.get_mut(event.0) {
+            Ok(q) => q,
+            Err(_) => {
+                entities_to_remove.0.push(event.0);
+                continue;
+            }
+        };
+        let mut curr_animation = animations.entities
+            .get_mut(&event.0)
+            .expect("Entity Not Found from `ResetAnimationEvent`")
+            .curr_animation
+            .lock()
+            .unwrap()
+        ;
+        // try and get the current animation
+        // if it is time based
+        if let Some(timed_animation) = curr_animation.timed_animation() {
+            timed_animation.reset_animation(Some(sprite), Some(direction));
+        }
+        // if it is transform based
+        else if let Some(transform_animation) = curr_animation.transform_animation() {
+            transform_animation.reset_animation(Some(sprite), Some(direction));
+        }
+        // if it is lineartime based
+        else if let Some(linear_timed_animation) = curr_animation.linear_timed_animation() {
+            linear_timed_animation.reset_animation(Some(sprite));
+        }
+        else {
+            panic!("Something went terribly wrong getting the current animation");
+        }
+    }
+}
+
+/// This system is for any cleanup of despawned entities
+fn remove_entites(
+    mut animations: ResMut<Animations>,
+    mut entities_to_remove: ResMut<EntitesToRemove>
+) {
+    for entity in entities_to_remove.0.iter() {
+        animations.entities.remove(&entity);
+    }
+    entities_to_remove.0.clear();
+}
---
+++ types.rs
@@ -0,0 +1,201 @@
+use crate::bevy_animations::*;
+use crate::*;
+
+pub type AnimationName = &'static str;
+
+/// This will determing which y index will be in the animating calculation
+/// So if we have a sprite sheet like [`this`](https://github.com/y0Phoenix/bevy_animations/blob/master/example%20sprites/example.png?raw=true)
+/// 
+/// Our code implementation will look like this
+/// ```
+/// AnimationDirectionIndexes::new(
+///     4,  // left
+///     3,  // right
+///     2,  // up
+///     1   // down
+/// );
+/// ```
+/// **Note** that bevy_animations uses a **1st index** basis for the DirectionIndexes instead of **0th index**
+/// 
+/// ###
+/// We can then take that code and call `insert_animation()` from the Animations Resource like this
+/// ```
+/// use bevy::prelude::*;
+/// use bevy_animations::*
+/// 
+/// fn setup_entity(
+///     mut commands: Commands,
+///     mut animations: ResMut<Animations>
+/// ) {
+///     let entity = commands.spawn(
+///         AnimationDirection::Still // the `AnimationDirection` component is needed on the entity to determine the direction
+///         SpriteSheetBundle {
+///             texture_atlas: // your sprite sheet handle
+///             transform: Transform::from_xyz(0., 0., 0.) // your desired location in the `World`
+///         }
+///         /* The rest of your entity configuration */
+///     );
+/// 
+///     animations.insert_animation(
+///         entity.id(),
+///         AnimationType::Transform(
+///             TransformAnimation::new(
+///                 /* animation_frames */ vec![0, 1, 2, 3] // the x index for your frames to cycle through
+///                 /* meters per frame */ 0.55 // your desired meters per frame
+///                 /* handle */ texture_atlas_hanle // your sprite sheet
+///                 /* frame */ Vec2::new(4., 4.) // the length and height of your sprite sheet
+///                 /* direction_indexes */ AnimationDirectionIndexes::new(4, 3, 2, 1) // from the example above
+///                 /* repeating */ true // if the animation is repeating or not
+///             )
+///         )
+///     )
+/// }
+/// ```
+/// 
+/// **Note** how the `animation_frames` field from the animation definition above is 0th index based
+#[derive(Clone, Debug)]
+pub struct AnimationDirectionIndexes {
+    pub left: usize,
+    pub right: usize,
+    pub up: usize,
+    pub down: usize,
+}
+
+impl AnimationDirectionIndexes {
+    pub fn new(left: usize, right: usize, up: usize, down: usize) -> Self{
+        Self { left, right, up, down }
+    }
+}
+
+impl Default for AnimationDirectionIndexes {
+    fn default() -> Self {
+        Self { left: 1, right: 1, up: 1, down: 1 }
+    }
+}
+
+#[derive(Debug, Component)]
+pub enum AnimationType {
+    // This is Primarily for This Is Primarily For Animations on players or NPCs, for example shooting a bow or reloading a gun 
+    Timed(TimedAnimation, AnimationName),
+    /// This Is Primarily For Animations on players or NPCs, for example walking or running
+    Transform(TransformAnimation, AnimationName),
+    /// This Is Primarily For Animations on objects, for example doors to open and close
+    LinearTimed(LinearTimedAnimation, AnimationName)
+}
+
+impl AnimationType {
+    pub fn get_atlas(&self) -> Handle<TextureAtlas> {
+        match self {
+            AnimationType::Timed(animation, _) => animation.handle.clone(),
+            AnimationType::Transform(animation, _) => animation.handle.clone(),
+            AnimationType::LinearTimed(animation, _) => animation.handle.clone(),
+        }
+    }
+    pub fn timed_animation(&mut self) -> Option<&mut TimedAnimation> {
+        match self {
+            AnimationType::Timed(timed_animation, _) => Some(timed_animation),
+            _ => None
+        }
+    }
+    pub fn linear_timed_animation(&mut self) -> Option<&mut LinearTimedAnimation> {
+        match self {
+            AnimationType::LinearTimed(linear_timed_animation, _) => Some(linear_timed_animation),
+            _ => None
+        }
+    }
+    pub fn transform_animation(&mut self) -> Option<&mut TransformAnimation> {
+        match self {
+            AnimationType::Transform(transform_animation, _) => Some(transform_animation),
+            _ => None
+        }
+    }
+    pub fn get_name(&self) -> &'static str {
+        match self {
+            AnimationType::Timed(_, name) => &name,
+            AnimationType::Transform(_, name) => &name,
+            AnimationType::LinearTimed(_, name) => &name,
+        }
+    }
+    pub fn reset_animation(&mut self) {
+        match self {
+            AnimationType::Timed(animation, _) => animation.reset_animation(None, None),
+            AnimationType::Transform(animation, _) => animation.reset_animation(None, None),
+            AnimationType::LinearTimed(animation, _) => animation.reset_animation(None),
+        }
+    }
+}
+
+/// Send a request to animate the `Entity` with the animation dictated by the `AnimationName`
+/// 
+/// # Example
+/// ```rust
+/// use bevy::prelude::*;
+/// use bevy_animations::*;
+/// 
+/// fn move_player(
+///     player_query: Query<(&Transform Entity), With<Player>>,
+///     mut animation_event_writer: EventWriter<AnimationEvent>
+/// ) {
+///     let (mut transform, player_entity) = player_query.single_mut();    
+/// 
+///     /* you move logic here... */
+/// 
+///     animation_event_writer.send(AnimationEvent("player_running", player_entity));
+/// }
+/// ```
+/// 
+/// * **Note** that you can send an event of the same name multiple times even while an animation is in progress without ruining it
+///
+/// * **Note** an animation that has been sent will animate till end or repeat forever
+#[derive(Debug)]
+pub struct AnimationEvent(pub AnimationName, pub Entity);
+
+/// Send a request to reset the animation of an `Entity`
+/// 
+/// # Example
+/// ```rust
+/// use bevy::prelude::*;
+/// use bevy_animations::*;
+/// 
+/// fn move_player(
+///     player_query: Query<(&Transform Entity), With<Player>>,
+///     mut reset_animation_event_writer: EventWriter<ResetAnimationEvent>
+/// ) {
+///     let (mut transform, player_entity) = player_query.single_mut();    
+/// 
+///     /* you move logic here... */
+/// 
+///     reset_animation_event_writer.send(ResetAnimationEvent(player_entity));
+/// }
+/// ```
+/// 
+/// * **Note** this will overwrite an animation request in the same frame
+#[derive(Debug)]
+pub struct ResetAnimationEvent(pub Entity);
+
+#[derive(Debug, PartialEq, Eq, Clone, Default, Component)]
+pub enum AnimationDirection {
+    Left,
+    Right,
+    Up,
+    Down,
+    #[default]
+    Still
+}
+
+impl AnimationDirection {
+    const LEFT: Vec2 = Vec2::new(-1., 0.);
+    const RIGHT: Vec2 = Vec2::new(1., 0.);
+    const UP: Vec2 = Vec2::new(0., 1.);
+    const DOWN: Vec2 = Vec2::new(0., -1.);
+    const STILL: Vec2 = Vec2::new(0., 0.);
+    pub fn get_direction(direction: &Self) -> Vec2 {
+        match direction {
+            AnimationDirection::Left => AnimationDirection::LEFT,
+            AnimationDirection::Right => AnimationDirection::RIGHT,
+            AnimationDirection::Up => AnimationDirection::UP,
+            AnimationDirection::Down => AnimationDirection::DOWN,
+            AnimationDirection::Still => AnimationDirection::STILL
+        }
+    }
+}
