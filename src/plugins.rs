df9eb80cee2d7fe6fb88402dc2762c243c56b3b8
Aaron <aarongraybill3@gmail.com>
2024-01-11 13:52:53 -0600

    fix: applied fixes to bevy 0.12

--- src/plugins.rs
+++ src/plugins.rs
@@ -84,7 +84,7 @@ fn catch_animation_events(
     mut commands: Commands
 ) {
     // our main event loop
-    for event in animation_events.iter() {
+    for event in animation_events.read() {
         // get the animating entity from the entity passed in from our event
         let animating_entity = animations.entities.get_mut(&event.1).expect(format!("Entity Not Found in Map For {} animation make sure your adding every necessary component to the entity i.e `AnimationDirection`", event.0).as_str());
         // query the texture the sprite and the current direction of the entity
@@ -258,7 +258,7 @@ fn catch_reset_events(
     mut entities_to_remove: ResMut<EntitesToRemove>,
     mut animation_events: EventReader<ResetAnimationEvent>
 ) {
-    for event in animation_events.iter() {
+    for event in animation_events.read() {
         // if the entity wasn't found in the query we want to remove it from our data structure
         let (sprite, direction) = match query.get_mut(event.0) {
             Ok(q) => q,
@@ -302,7 +302,7 @@ fn catch_fx_animation_events(
     mut commands: Commands,
     mut animations: ResMut<Animations>
 ) {
-    for event in event_reader.iter() {
+    for event in event_reader.read() {
         let entity = commands.spawn(AnimationDirection::default()).id();
         let Ok(sprite_sheet_bundle) = animations.start_fx_animation(entity, event.0, event.1) else { 
             warn!("There was a problem spawning your FXAnimation {}", event.0);
